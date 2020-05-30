158e97862cdfa3ae2d5761f0965673fdc17015de
Lucas Scherpel <lucasscherpel@gmail.com>
2020-05-30 20:03:55 -0300

    footer implemented

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Background from "./media/desktop.mp4";
+import Footer from "./pages/Footer/Footer"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <video autoPlay loop style={{
+      <video autoPlay loop style={{
             position: "absolute",
             width: "100%",
-            height: "100%",
+            height: "110%",
             zIndex: "-1",
             objectFit: "cover"
 
           }}>
             <source src={Background}/>
           </video>
+      
           <div className="logon-container">
 
             <section className="form">
-                    <h1>Bem-vindo. <br/>Aqui renasce a Criatividade Brasileira.</h1>
-                    <button className="button" type="submit">ONBOARDING</button>
-                    <button className="button" type="submit">REBORN</button>
-                    {/* <Link to="/register" className="back-link">
-                        <FiLogIn size={16} color="#E02041"/>
-                         Não tenho cadastro</Link> */}
+              <h1>Bem-vindo. <br/>Aqui renasce a Criatividade Brasileira.</h1>
+              <button className="button" type="submit">ONBOARDING</button>
+              <button className="button" type="submit">REBORN</button>
+
+              {/* <Link to="/register" className="back-link">
+                <FiLogIn size={16} color="#E02041"/>
+                Não tenho cadastro</Link> */}
+                         
             </section>
             {/* <img src={HeroesImg} alt="header image"/> */}
 
         </div>
+        <Footer/>
+
 
           {/* <h2 key="h2">Bem-vindo.<br/>Aqui renasce <br/> a Criatividade <br/> Brasileira. </h2>
           <div key="button1" className="start-button clearfix">
@@ -53,6 +59,7 @@ function App() {
           Learn React
         </a> */}
       </header>
+
     </div>
   );
 }
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+import Wave from '../../media/waveVideo.mp4'
+
+export default function Footer(){
+    return(
+       <div> 
+           <footer className= "footer">
+           <a href="http://instagram.com/rebornbr">Follow us on Instagram</a>
+            <video autoPlay loop style={{
+                width: "50px",
+            }}>
+            <source src={Wave}/>
+          </video>
+      
+            </footer>
+       </div>
+       
+    );
+};
